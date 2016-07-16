60a69f1b711ff0d9b329c4425da71ae97d93f421
SwittS <switt.srikulacheep@gmail.com>
2016-07-16 14:46:06 -0700

    add ajax to allow creation of new dancemove from html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,41 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  $.ajax({
+    method: 'GET',
+    url: '/api/dancemoves',
+    success: handleSuccess,
+    error: handleError
+  });
 
+  $('#newDancemove').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/dancemoves',
+      data: $(this).serialize(),
+      success: newDancemoveSuccess,
+      error: newDancemoveError
+    });
+
+    function handleSuccess(json) {
+      allDancemoves = json;
+      render();
+    }
+
+    function handleError(e) {
+      console.log('uh oh');
+      $('#newDancemove').text('Failed to load dancemoves, is the server working?');
+    }
+
+    function newDancemoveSuccess(json) {
+      $('newDancemove').val('');
+      allDancemoves.push(json);
+      render();
+    }
+
+    function newDancemoveError() {
+
+    }
+  });
 });
